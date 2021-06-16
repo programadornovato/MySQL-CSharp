8bfc54d45f7623292644262a7b57ee67a347d4da
Programador Novato <info@programadornovato.com>
2021-06-16 17:11:30 -0500

    Conectar C# a MySQL (MariaDB) [02]

--- MySQL-CSharp/Form1.Designer.cs
+++ MySQL-CSharp/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace MySQL_CSharp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(96, 53);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(99, 39);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(96, 122);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(206, 25);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Estado de la conexion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(543, 523);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConectar);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- MySQL-CSharp/Form1.cs
+++ MySQL-CSharp/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace MySQL_CSharp
 {
     public partial class Form1 : Form
@@ -17,5 +17,22 @@ namespace MySQL_CSharp
             InitializeComponent();
         }
 
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string server = "localhost";
+                string database = "empleados";
+                string user = "root";
+                string pwd = "123456";
+                string cadenaConexion = "server=" + server + ";database=" + database + ";" + "Uid=" + user + ";pwd=" + pwd + ";";
+                MySqlConnection myCon = new MySqlConnection(cadenaConexion);
+                myCon.Open();
+                lblResultado.Text = "Conexion exitosa";
+            }
+            catch (Exception error) {
+                lblResultado.Text = "Error de conexion "+ error;
+            }
+        }
     }
 }
