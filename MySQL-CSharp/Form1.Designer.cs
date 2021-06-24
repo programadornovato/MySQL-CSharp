fea0cb8e95eb7517b3924a4bec010e673da75bce
Programador Novato <info@programadornovato.com>
2021-06-24 02:27:38 -0500

    Buscar registros en MySQL (MariaDB) con C# [04] video: https://youtu.be/1BkU3tG1KEo

--- MySQL-CSharp/Form1.Designer.cs
+++ MySQL-CSharp/Form1.Designer.cs
@@ -29,30 +29,26 @@ namespace MySQL_CSharp
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnConectar = new System.Windows.Forms.Button();
             this.lblResultado = new System.Windows.Forms.Label();
             this.dgTrabajadores = new System.Windows.Forms.DataGridView();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.puesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBusNombre = new System.Windows.Forms.TextBox();
+            this.txtBusPuesto = new System.Windows.Forms.TextBox();
+            this.txtBusEdad = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgTrabajadores)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnConectar
-            // 
-            this.btnConectar.Location = new System.Drawing.Point(96, 53);
-            this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(99, 39);
-            this.btnConectar.TabIndex = 0;
-            this.btnConectar.Text = "Conectar";
-            this.btnConectar.UseVisualStyleBackColor = true;
-            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
             // 
             // lblResultado
             // 
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(96, 122);
+            this.lblResultado.Location = new System.Drawing.Point(348, 598);
             this.lblResultado.Name = "lblResultado";
             this.lblResultado.Size = new System.Drawing.Size(206, 25);
             this.lblResultado.TabIndex = 1;
@@ -66,10 +62,10 @@ namespace MySQL_CSharp
             this.Nombre,
             this.puesto,
             this.Edad});
-            this.dgTrabajadores.Location = new System.Drawing.Point(62, 248);
+            this.dgTrabajadores.Location = new System.Drawing.Point(45, 338);
             this.dgTrabajadores.Name = "dgTrabajadores";
             this.dgTrabajadores.RowTemplate.Height = 25;
-            this.dgTrabajadores.Size = new System.Drawing.Size(458, 243);
+            this.dgTrabajadores.Size = new System.Drawing.Size(509, 243);
             this.dgTrabajadores.TabIndex = 2;
             // 
             // id
@@ -92,14 +88,78 @@ namespace MySQL_CSharp
             this.Edad.HeaderText = "edad";
             this.Edad.Name = "Edad";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Puesto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(300, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Edad";
+            // 
+            // txtBusNombre
+            // 
+            this.txtBusNombre.Location = new System.Drawing.Point(45, 296);
+            this.txtBusNombre.Name = "txtBusNombre";
+            this.txtBusNombre.Size = new System.Drawing.Size(100, 33);
+            this.txtBusNombre.TabIndex = 4;
+            // 
+            // txtBusPuesto
+            // 
+            this.txtBusPuesto.Location = new System.Drawing.Point(170, 296);
+            this.txtBusPuesto.Name = "txtBusPuesto";
+            this.txtBusPuesto.Size = new System.Drawing.Size(100, 33);
+            this.txtBusPuesto.TabIndex = 4;
+            // 
+            // txtBusEdad
+            // 
+            this.txtBusEdad.Location = new System.Drawing.Point(300, 296);
+            this.txtBusEdad.Name = "txtBusEdad";
+            this.txtBusEdad.Size = new System.Drawing.Size(100, 33);
+            this.txtBusEdad.TabIndex = 4;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(440, 288);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(114, 41);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 523);
+            this.ClientSize = new System.Drawing.Size(583, 655);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBusEdad);
+            this.Controls.Add(this.txtBusPuesto);
+            this.Controls.Add(this.txtBusNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgTrabajadores);
             this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.btnConectar);
             this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -110,14 +170,19 @@ namespace MySQL_CSharp
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnConectar;
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.DataGridView dgTrabajadores;
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn puesto;
         private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBusNombre;
+        private System.Windows.Forms.TextBox txtBusPuesto;
+        private System.Windows.Forms.TextBox txtBusEdad;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
 
--- MySQL-CSharp/Form1.cs
+++ MySQL-CSharp/Form1.cs
@@ -16,6 +16,8 @@ namespace MySQL_CSharp
         public Form1()
         {
             InitializeComponent();
+            conectar();
+            llenarTabla();
         }
         private void conectar() {
             try
@@ -34,14 +36,13 @@ namespace MySQL_CSharp
                 lblResultado.Text = "Error de conexion " + error;
             }
         }
-        private void btnConectar_Click(object sender, EventArgs e)
-        {
-            conectar();
+        private void llenarTabla() {
             string query = "select id,nombre,puesto,edad from trabajadores;";
             MySqlCommand comandoDB = new MySqlCommand(query, myCon);
             comandoDB.CommandTimeout = 60;
             MySqlDataReader reader;
-            try {
+            try
+            {
                 reader = comandoDB.ExecuteReader();
                 if (reader.HasRows)
                 {
@@ -55,11 +56,66 @@ namespace MySQL_CSharp
                         //MessageBox.Show(reader.GetString(0));
                     }
                 }
-                else {
+                else
+                {
                     Console.WriteLine("No hay trabajadores");
                 }
+                reader.Close();
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string where = " where 1=1 ";
+            //Si el nombre no esta vacio
+            if (txtBusNombre.Text != "")
+            {
+                where = where + " and nombre like'%" + txtBusNombre.Text + "%' ";
+            }
+            //Si el puesto no esta vacio
+            if (txtBusPuesto.Text != "")
+            {
+                where = where + " and puesto like'%" + txtBusPuesto.Text + "%' ";
+            }
+            //Si la edad no esta vacio
+            if (txtBusEdad.Text != "")
+            {
+                where = where + " and edad like'%" + txtBusEdad.Text + "%' ";
+            }
+            string query = "select id,nombre,puesto,edad from trabajadores " + where + " ;";
+            MySqlCommand comandoDB = new MySqlCommand(query, myCon);
+            comandoDB.CommandTimeout = 60;
+            MySqlDataReader reader;
+            dgTrabajadores.Rows.Clear();
+            dgTrabajadores.Refresh();
+            try
+            {
+                reader = comandoDB.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int n = dgTrabajadores.Rows.Add();
+                        dgTrabajadores.Rows[n].Cells[0].Value = reader.GetString(0);
+                        dgTrabajadores.Rows[n].Cells[1].Value = reader.GetString(1);
+                        dgTrabajadores.Rows[n].Cells[2].Value = reader.GetString(2);
+                        dgTrabajadores.Rows[n].Cells[3].Value = reader.GetString(3);
+                        //MessageBox.Show(reader.GetString(0));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No hay trabajadores");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex);
             }
         }
